329676cc76834fc32816ed9eb032ec3e45d0171c
Iair Kaplun <i.kaplun@viseven.com>
2025-10-15 21:07:24 -0300

    actualizacion sobre la creacion de los csv de garages

--- constantes/tipos_vehiculos.py
+++
@@ -1,6 +0,0 @@
-def enum_tipo_vehiculo(type_id):
-    return {
-        1: "Moto",
-        2: "Auto",
-        3: "Camioneta",
-    }
--- visual/menu_handlers.py
+++ visual/menu_handlers.py
@@ -6,7 +6,9 @@ from users.usuarios import user_login, registrar_nuevo_usuario
 from users.users_garage import (
     buscar_garage_asociado,
     seleccionar_solo_un_garage,
-    asociar_garage_a_usuario
+    asociar_garage_a_usuario, 
+    crear_archivo_users_garage, 
+    crear_garage
 )
 
 def handle_login():
@@ -31,42 +33,11 @@ def handle_registro():
 def crear_nuevo_garage(usuario):
     """Interfaz para crear un nuevo garage"""
     print("\n=== CREAR NUEVO GARAGE ===")
-    nombre = input("Nombre del garage: ")
-    direccion = input("Dirección: ")
+    #Por ahora me lo esta agregando sen  el file users-garage.csv
+    garage = crear_garage(usuario)
     
-    # Validar pisos
-    pisos_validos = False
-    while not pisos_validos:
-        try:
-            pisos = int(input("Cantidad de pisos: "))
-            if pisos > 0:
-                pisos_validos = True
-            else:
-                print("Debe ser mayor a 0")
-        except ValueError:
-            print("Ingrese un número válido")
     
-    # Validar slots por piso
-    slots_validos = False
-    while not slots_validos:
-        try:
-            slots_por_piso = int(input("Slots por piso: "))
-            if slots_por_piso > 0:
-                slots_validos = True
-            else:
-                print("Debe ser mayor a 0")
-        except ValueError:
-            print("Ingrese un número válido")
-    
-    asociar_garage_a_usuario(
-        usuario['email'], 
-        nombre, 
-        direccion, 
-        pisos, 
-        slots_por_piso
-    )
-    
-    return True
+    return garage
 
 def handle_seleccionar_garage(usuario):
     """Maneja la selección de garage existente"""
