77b1c6ea3b76be4b79570954ab3ae9160f12019b
Emma Aprile <eaprile@uade.edu.ar>
2025-10-27 13:53:54 -0300

    Confirmo mis cambios

--- auxiliares/consola.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-def clear_screen():
-    """ Limpia la pantalla de la consola """    
-    if sys.platform.startswith('win'):
-            os.system('cls')  # For Windows
-    else:
-            os.system('clear') # For Linux and macOS
--- constantes/tipos_vehiculos.py
+++
@@ -1,7 +0,0 @@
-def enum_tipo_vehiculo():
-    return {
-        "moto": 1 ,
-        "auto": 2,
-        "camioneta": 3,
-        "bicicleta": 4
-    }
